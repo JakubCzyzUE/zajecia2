6fa0da979c0f9e0eb7c2b682aee2f6dfdf039b6d
Jakub Cz <92373216+JakubCzyzUE@users.noreply.github.com>
2021-10-19 14:33:03 +0200

    return added

--- lab_2zadanie2/main.py
+++ lab_2zadanie2/main.py
@@ -3,9 +3,12 @@ def function(list = []):
     for x in list:
        x = x*2
        print(x)
+    return list
+
 def function2(list = []):
     list_2 = [ x*2 for x in list]
     print(list_2)
+    return list_2
 
 
 
