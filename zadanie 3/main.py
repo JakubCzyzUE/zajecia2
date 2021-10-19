026e528d5c3ba8fb6cc849599df7748729a80d8c
Jakub Cz <92373216+JakubCzyzUE@users.noreply.github.com>
2021-10-19 14:49:08 +0200

    zadanie 4

--- zadanie 3/main.py
+++ zadanie 3/main.py
@@ -1,8 +1,9 @@
-numbers = [1,2,3,4,5,6,7,8,9,10]
-numbers_len = len(numbers)
+numbers = []
 
 
-for x in numbers:
-    if(x % 2 == 0):
-        print(x)
+def function(list=[]):
+    for i in range(11):
+        if(i % 2 == 0):
+            print(i)
 
+function(numbers)
