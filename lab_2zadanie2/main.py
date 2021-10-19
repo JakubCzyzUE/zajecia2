74a668a5c5e4fc480ff9bf2c99d1cfa89efa912f
Jakub Cz <92373216+JakubCzyzUE@users.noreply.github.com>
2021-10-19 14:31:59 +0200

    dodany podpunkt B

--- lab_2zadanie2/main.py
+++ lab_2zadanie2/main.py
@@ -1,10 +1,15 @@
 
-def function(list =[]):
+def function(list = []):
     for x in list:
        x = x*2
        print(x)
+def function2(list = []):
+    list_2 = [ x*2 for x in list]
+    print(list_2)
+
 
 
 integers = [10,20,30]
 
 function(integers)
+function2(integers)
